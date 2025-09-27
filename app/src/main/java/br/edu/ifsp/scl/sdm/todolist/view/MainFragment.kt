edb3d3dbda31ae3c0d73d89ec5fc9daf0092a24a
RafaelaRamos <rafaellapereyra@gmail.com>
2025-09-27 14:51:51 -0300

    feat:Implementando arquitetura MVVM

--- app/src/main/java/br/edu/ifsp/scl/sdm/todolist/view/MainFragment.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/todolist/view/MainFragment.kt
@@ -11,12 +11,14 @@ import android.view.inputmethod.InputMethodManager.HIDE_NOT_ALWAYS
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.sdm.todolist.R
 import br.edu.ifsp.scl.sdm.todolist.controller.MainController
 import br.edu.ifsp.scl.sdm.todolist.controller.TaskPresenter
+import br.edu.ifsp.scl.sdm.todolist.controller.TaskViewModel
 import br.edu.ifsp.scl.sdm.todolist.databinding.FragmentMainBinding
 import br.edu.ifsp.scl.sdm.todolist.model.entity.Task
 import br.edu.ifsp.scl.sdm.todolist.model.entity.Task.Companion.TASK_DONE_FALSE
@@ -24,7 +26,7 @@ import br.edu.ifsp.scl.sdm.todolist.model.entity.Task.Companion.TASK_DONE_TRUE
 import br.edu.ifsp.scl.sdm.todolist.view.adapter.OnTaskClickListener
 import br.edu.ifsp.scl.sdm.todolist.view.adapter.TaskAdapter
 
-class MainFragment : Fragment(), OnTaskClickListener,TaskPresenter.TaskView {
+class MainFragment : Fragment(), OnTaskClickListener {
     private lateinit var fmb: FragmentMainBinding
 
     // Data source
@@ -45,10 +47,13 @@ class MainFragment : Fragment(), OnTaskClickListener,TaskPresenter.TaskView {
         const val EXTRA_TASK = "EXTRA_TASK"
         const val TASK_FRAGMENT_REQUEST_KEY = "TASK_FRAGMENT_REQUEST_KEY"
     }
-    // Presenter
-    private val taskPresenter: TaskPresenter by lazy {
-      TaskPresenter(this)
+
+    // ViewModel
+    private val taskViewModel: TaskViewModel by viewModels {
+        TaskViewModel.taskViewModelFactory
+
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,11 +68,11 @@ class MainFragment : Fragment(), OnTaskClickListener,TaskPresenter.TaskView {
                 task?.also { receivedTask ->
                     taskList.indexOfFirst { it.time == receivedTask.time }.also { position ->
                         if (position != -1) {
-                            taskPresenter.editTask(receivedTask)
+                            taskViewModel.editTask(receivedTask)
                             taskList[position] = receivedTask
                             tasksAdapter.notifyItemChanged(position)
                         } else {
-                            taskPresenter.insertTask(receivedTask)
+                            taskViewModel.insertTask(receivedTask)
                             taskList.add(receivedTask)
                             tasksAdapter.notifyItemInserted(taskList.lastIndex)
                         }
@@ -81,7 +86,15 @@ class MainFragment : Fragment(), OnTaskClickListener,TaskPresenter.TaskView {
                 )
             }
         }
-        taskPresenter.getTasks()
+
+        taskViewModel.taskMld.observe(requireActivity()) { tasks ->
+            taskList.clear()
+            tasks.forEachIndexed { index, task ->
+                taskList.add(task)
+                tasksAdapter.notifyItemChanged(index)
+            }
+        }
+        taskViewModel.getTasks()
     }
 
     override fun onCreateView(
@@ -107,7 +120,7 @@ class MainFragment : Fragment(), OnTaskClickListener,TaskPresenter.TaskView {
     override fun onTaskClick(position: Int) = navigateToTaskFragment(position, false)
 
     override fun onRemoveTaskMenuItemClick(position: Int) {
-        taskPresenter.removeTask(taskList[position])
+        taskViewModel.removeTask(taskList[position])
         taskList.removeAt(position)
         tasksAdapter.notifyItemRemoved(position)
     }
@@ -117,7 +130,7 @@ class MainFragment : Fragment(), OnTaskClickListener,TaskPresenter.TaskView {
     override fun onDoneCheckBoxClick(position: Int, checked: Boolean) {
         taskList[position].apply {
             done = if (checked) TASK_DONE_TRUE else TASK_DONE_FALSE
-            taskPresenter.editTask(this)
+            taskViewModel.editTask(this)
         }
     }
 
@@ -129,13 +142,4 @@ class MainFragment : Fragment(), OnTaskClickListener,TaskPresenter.TaskView {
         }
     }
 
-   override fun updateTaskList(tasks: List<Task>) {
-        taskList.clear()
-        tasks.forEachIndexed { index, task ->
-            taskList.add(task)
-            tasksAdapter.notifyItemChanged(index)
-
-        }
-
-    }
 }
